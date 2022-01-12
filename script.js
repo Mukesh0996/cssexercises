a064745cc108611e136b85152cdaf4855d4dc23f
Mukesh <mukeshkeerthipati96@gmail.com>
2022-01-12 13:01:26 +0530

    added product card layout

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const navTwo_listItems = document.querySelectorAll(".navigation_two .list");
 const inpBox = document.querySelector(".input");
 const inputField = document.querySelector(".input input");
 const inputOptionElements = document.querySelectorAll(".option div");
+const search_container = document.querySelector(".search_container");
+const search_container_Input = document.querySelector(".search_container input");
 
+const drag = document.querySelector(".drag");
 let $ = (element, eventType, fns) => element.addEventListener(eventType, fns);
 
 function activeLinkOne () {
-    navOne_listItems.forEach(item =>{
+    navOne_listItems.forEach(item => {
         item.classList.remove("active");
         this.classList.add("active");
     })
@@ -36,3 +39,39 @@ function insertValue () {
 }
 
 inputOptionElements.forEach(elem => $(elem, "click", insertValue));
+
+$(search_container, "click", function (e) {
+    if(e.currentTarget.classList.contains("activate")) {
+       return;
+    } else {
+        e.currentTarget.classList.add("activate");
+    }
+});
+
+function moveElement (e) {
+    let getStyle = window.getComputedStyle(drag);
+    let leftVal = parseInt(getStyle.left);
+    let topVal = parseInt(getStyle.top);
+    drag.style.left = `${leftVal + e.movementX}px`;
+    drag.style.top = `${topVal + e.movementY}px`;
+}
+
+
+drag.addEventListener("mousedown", ()=> {
+    drag.addEventListener("mousemove", moveElement)
+});
+
+document.addEventListener("mouseup", () => {
+    drag.removeEventListener("mousemove", moveElement);
+});
+
+document.addEventListener("mousedown", (e) => {
+    if(e.target ===search_container_Input || e.target === search_container) {
+        return
+    }
+    if(search_container_Input.value !== "") {
+        return;
+    } else if (search_container_Input.value === "") {
+        search_container.classList.remove("activate");
+    }
+})
